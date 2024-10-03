2d68a1a22244ba211f9d114d1c41053a53fda16e
ziyangfan <1229417754@qq.com>
2024-10-03 10:56:06 +0800

    replace upsample+conv with transposed conv

--- DCGAN/dcgan.py
+++ DCGAN/dcgan.py
@@ -22,10 +22,11 @@ class Generator(nn.Module):
         self.fc = nn.Linear(latent_dim, 1024 * 2 * 2)
         self.convs = nn.Sequential(
             nn.BatchNorm2d(1024),
-            nn.Upsample(scale_factor=2), nn.Conv2d(1024, 512, kernel_size=3, padding=1), nn.BatchNorm2d(512), nn.ReLU(),
-            nn.Upsample(scale_factor=2), nn.Conv2d(512, 256, kernel_size=3, padding=1), nn.BatchNorm2d(256), nn.ReLU(),
-            nn.Upsample(scale_factor=2), nn.Conv2d(256, 128, kernel_size=3, padding=1), nn.BatchNorm2d(128), nn.ReLU(),
-            nn.Upsample(scale_factor=2), nn.Conv2d(128, num_channels, kernel_size=3, padding=1), nn.Tanh())
+            nn.ConvTranspose2d(1024, 512, kernel_size=5, stride=2, padding=2, output_padding=1), nn.BatchNorm2d(512), nn.ReLU(),
+            nn.ConvTranspose2d(512, 256, kernel_size=5, stride=2, padding=2, output_padding=1), nn.BatchNorm2d(256), nn.ReLU(),
+            nn.ConvTranspose2d(256, 128, kernel_size=5, stride=2, padding=2, output_padding=1), nn.BatchNorm2d(128), nn.ReLU(),
+            nn.ConvTranspose2d(128, num_channels, kernel_size=5, stride=2, padding=2, output_padding=1), nn.Tanh())
+
     
     def forward(self, z: Tensor) -> Tensor:
         """Forward pass in Generator.
@@ -51,12 +52,11 @@ class Discriminator(nn.Module):
         """
         super(Discriminator, self).__init__()
         self.model = nn.Sequential(
-            nn.Conv2d(num_channels, 128, kernel_size=3, stride=2, padding=1), nn.LeakyReLU(0.2),
-            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1), nn.BatchNorm2d(256), nn.LeakyReLU(0.2),
-            nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1), nn.BatchNorm2d(512), nn.LeakyReLU(0.2),
-            nn.Conv2d(512, 1024, kernel_size=3, stride=2, padding=1), nn.BatchNorm2d(1024), nn.LeakyReLU(0.2),
-            nn.Flatten(),
-            nn.Linear(1024 * 2 * 2, 1), nn.Sigmoid())
+            nn.Conv2d(num_channels, 128, kernel_size=5, stride=2, padding=2), nn.LeakyReLU(0.2),
+            nn.Conv2d(128, 256, kernel_size=5, stride=2, padding=2), nn.BatchNorm2d(256), nn.LeakyReLU(0.2),
+            nn.Conv2d(256, 512, kernel_size=5, stride=2, padding=2), nn.BatchNorm2d(512), nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 1024, kernel_size=5, stride=2, padding=2), nn.BatchNorm2d(1024), nn.LeakyReLU(0.2),
+            nn.Flatten(), nn.Linear(1024 * 2 * 2, 1), nn.Sigmoid())
     
     def forward(self, images: Tensor) -> Tensor:
         """Forward pass in Discriminator.
